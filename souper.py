15a2cea9c9f7b25c7b8ae4e641395ee7787183b2
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-23 18:24:07 +0100

    Initial sauce

---
+++ souper.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+from collections import defaultdict
+import sys, codecs
+import os
+
+search_index = defaultdict(set)
+with open(os.path.join(os.path.dirname(__file__), 'stopwords.txt'), 'r') as f:
+    stop_words = set(f.read().split())
+    print stop_words
+
+def filter_char(char):
+    return ord('a') <= ord(char) <= ord('z') or char == '.' or char == '_'
+
+def token_filter(token):
+    token = token.lower()
+    token = ''.join(i for i in token if filter_char(i))
+
+    if token in stop_words:
+        return None
+
+    if not token:
+        return None
+
+    return token
+
+
+def parse_file(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+
+    soup = BeautifulSoup(data, 'lxml')
+    main_content = soup.find('div', {'class': 'col-md-8'})
+    anchors = main_content.find_all(lambda tag: tag.has_attr('id'))
+    for anchor in anchors:
+        anchor_id = anchor.attrs['id']
+        all_text = ''.join(anchor.findAll(text=True))
+        tokens = all_text.split()
+        for token in tokens:
+            filtered = token_filter(token)
+            if filtered is None:
+                continue
+            search_index[token].add(anchor_id.strip())
+
+if __name__=='__main__':
+    for root, dirs, files in os.walk(sys.argv[1]):
+        for file in files:
+            if file.endswith(".html"):
+                print(os.path.join(root, file)) 
+                parse_file(os.path.join(root, file))
+                
+    with codecs.open('search_index.txt', 'w', 'utf-8') as f:
+        for key, value in search_index.items():
+            f.write (key)
+            for elem in value:
+                f.write(' ' + elem)
+            f.write('\n')
