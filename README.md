b1c9841a9a34d53856487cee0ada1e073a2141c9
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-03-07 13:34:50 +0100

    License hotdoc's search extension to LGPLv2.1+

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Based on the algorithms presented by http://stevehanov.ca/blog/, copyright attribution
 to steve hanov.
+
+### Licensing
+
+hotdoc's search extension is licensed under the LGPL version 2.1 (or, at your option, any
+later version). See COPYING for more details.
--- javascript/search.js
+++ javascript/search.js
@@ -1,3 +1,24 @@
+/*
+ * Search hook for hotdoc's search extension.
+ *
+ * Copyright 2015,2016 Mathieu Duponchelle <mathieu.duponchelle@opencredd.com>
+ * Copyright 2015,2016 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 var trie = undefined;
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
